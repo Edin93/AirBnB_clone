b66927479746862673efe5e7a2f5d9310648d9be
edin93 <il_matador@hotmail.fr>
2020-02-17 12:00:05 +0100

    task 9 done

--- console.py
+++ console.py
@@ -2,12 +2,18 @@
 """
 Module that contains the entry point of the command interpreter
 """
+from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 import cmd
 import inspect
-from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
+from models.place import Place
+from models.review import Review
 from models import storage
+from models.state import State
 from models.user import User
+
 
 class HBNBCommand(cmd.Cmd):
     """
