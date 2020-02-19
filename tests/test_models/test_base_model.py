0c23a6b6b477678f2e0ea11a917a972781e8ab4a
edin93 <il_matador@hotmail.fr>
2020-02-19 21:39:05 +0100

    basemodel tests updated

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -19,12 +19,20 @@ class test_file(unittest.TestCase):
         """
         bs1 = BaseModel()
         bs2 = BaseModel()
+        self.assertTrue(isinstance(bs1, BaseModel))
         self.assertNotEqual(bs1.id, bs2.id)
         bs1.name = "Holberton"
         self.assertEqual(bs1.name, "Holberton")
+        self.assertTrue(isinstance(bs1.name, str))
         bs1.age = 5
         self.assertEqual(bs1.age, 5)
+        self.assertTrue(isinstance(bs1.age, int))
         self.assertTrue(isinstance(bs1.created_at, datetime.datetime))
         self.assertTrue(isinstance(bs1.updated_at, datetime.datetime))
         self.assertTrue(isinstance(bs1.id, str))
         self.assertTrue(isinstance(bs1.to_dict(), dict))
+        self.assertTrue(hasattr(bs1, 'id'))
+        self.assertTrue(hasattr(bs1, 'created_at'))
+        self.assertTrue(hasattr(bs1, 'updated_at'))
+        self.assertTrue(hasattr(bs1, 'age'))
+        self.assertTrue(hasattr(bs1, 'name'))
